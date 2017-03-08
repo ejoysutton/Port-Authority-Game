72908812968b623d93233209a1923dd31551b42a
ejoysutton <joy.ericka.sutton@gmail.com>
2017-03-08 11:23:13 -0500

    Added gameValues const with named vars to store game elements, and onclick handler for game start.

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 //Handle startButton click
 $("#startButton").click(function() {
 	console.log("Start clicked!")
+	gameValues.gameStarted = true;
 });
+
+const gameValues = {
+  PLACEHOLDER: function(){
+    if (gameValues.gameStarted) {
+
+    }
+  },
+  gameStarted: false,
+  timerValue: 0,
+  targetScore: 0,
+  playerScore: 0,
+  playerWin: false,
+  // advanceTenMillisecs: function(){
+  //   this.millisecs += 10;
+  //   if (this.millisecs >= 1000) {
+  //       this.millisecs -= 1000;
+  //       this.secs ++;
+  //   }
+  //   if (this.secs >= 60) {
+  //       this.secs -= 60;
+  //       this.mins ++;
+  //   }
+  }
+
